e59b24b66d4c38763829fb4e7d122e7d87164948
你好呀 <111191349+88lin@users.noreply.github.com>
2023-05-29 16:40:33 +0800

    Update handle-request.ts

--- src/handle-request.ts
+++ src/handle-request.ts
@@ -1,29 +1,20 @@
-const blacklist = ["112.43.140.86", "1.68.91.94"];
-
-const CORS_HEADERS = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers":
-    "Content-Type, Authorization, X-Requested-With",
-};
-
 const pickHeaders = (headers: Headers, keys: (string | RegExp)[]): Headers => {
   const picked = new Headers();
   for (const key of headers.keys()) {
     if (keys.some((k) => (typeof k === "string" ? k === key : k.test(key)))) {
-        // 获取客户端IP地址
-        const ip = headers.get("cf-connecting-ip") || headers.get("x-forwarded-for") || headers.get("remote-addr");
-        // 检查IP地址是否在黑名单中
-        if (ip && blacklist.includes(ip)) {
-            return new Response(null, { status: 403 });
-        }
-        const value = headers.get(key);
-        if (typeof value === "string") {
-          picked.set(key, value);
-        }
+      const value = headers.get(key);
+      if (typeof value === "string") {
+        picked.set(key, value);
       }
     }
-    return picked;
+  }
+  return picked;
+};
+
+const CORS_HEADERS: Record<string, string> = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "Content-Type, Authorization",
 };
 
 export default async function handleRequest(req: Request & { nextUrl?: URL }) {
@@ -35,12 +26,7 @@ export default async function handleRequest(req: Request & { nextUrl?: URL }) {
 
   const { pathname, search } = req.nextUrl ? req.nextUrl : new URL(req.url);
   const url = new URL(pathname + search, "https://api.openai.com").href;
-
   const headers = pickHeaders(req.headers, ["content-type", "authorization"]);
-
-  if (headers.status === 403) {
-    return new Response(null, { status: 403 });
-}
 
   const res = await fetch(url, {
     body: req.body,
